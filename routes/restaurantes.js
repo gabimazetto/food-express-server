39d7624c07a8ecac5860e099a151a0c0b1de3d53
ZamboniA <andersonzamboni18@gmail.com>
2023-05-08 17:35:46 -0300

    Implementação do método PUT de restaurantes e sua rota

--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -19,8 +19,30 @@ router.post("/restaurantes", async (req, res) => {
     } catch (err) {
         res.status(500).send('Ocorreu um erro. Por favor, tente novamente mais tarde.');
     }
-})
+});
 
+
+//PUT
+router.put("/restaurantes/:id", async (req, res) =>{
+    const { nomeFantasia, razaoSocial, cnpj, email, senha, endereco } = req.body;
+    const { id } = req.params;
+
+    try {
+        const restauranteAtt = await Restaurante.findByPk(id);
+        
+        if(restauranteAtt) {
+            if(endereco) {
+                await Endereco.update(endereco, { where: { restauranteId: id } })
+            }
+            await restauranteAtt.update({ nomeFantasia, razaoSocial, cnpj, email, senha });
+            res.status(200).json({ message: "Restaurante editado."})
+        } else {
+            res.status(404).json({ message: "Restaurante não encontrado" })
+        }
+    } catch (err) {
+        res.status(500).send('Ocorreu um erro. Por favor, tente novamente mais tarde.');
+    }
+});
 
 
 module.exports = router;
