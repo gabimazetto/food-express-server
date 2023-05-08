10678e48bde80a2cdc417cd8615105de9ee37933
jhonasjhones <jhonajhone222@gmail.com>
2023-05-08 18:00:20 -0300

    funcionalidade de atualizar comida

--- routes/comidas.js
+++ routes/comidas.js
@@ -43,6 +43,22 @@ router.get("/comidas/:id", async (req, res) => {
   }
 });
 
+// ROTA PARA EDITAR COMIDA
+router.put("/comidas/:id", async (req, res) => {
+  const { codigo, nome, descricao, categoria, preco, peso, imagem } = req.body;
+  const { id } = req.params;
+  try{
+    const comida = await Comida.findByPk(id);
 
+    if(comida) {
+    await comida.update({codigo, nome, descricao, categoria, preco, peso, imagem })
+    res.status(200).json({message: "Comida editada."})
+    } else {
+      res.status(404).json({message:"Comida não encontrada."})
+    }
+  } catch (err) {
+    res.status(500).json("Ocorreu um erro.")
+  }
+})
 
 module.exports = router;
