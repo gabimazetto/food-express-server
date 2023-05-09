87403153300aa0591a137ba895e01f87cd53f1af
juliagascho <juliagascho@hotmail.com>
2023-05-08 21:26:41 -0300

    Rota para remoção de cliente finalizada - DELETE

--- routes/clientes.js
+++ routes/clientes.js
@@ -8,76 +8,91 @@ const router = Router();
 
 // Definição de rotas
 
-// POST
-router.post("/clientes", async (req, res) =>{
-    // Coletar dados do req.body
-    const { nome, email, senha, telefone, cpf, dataNascimento, endereco } = req.body;
-
+// POST - ROTA PARA ADICIONAR UM CLIENTE
+router.post("/clientes", async (req, res) => {
+    const { nome, email, senha, telefone, cpf, dataNascimento, endereco } =
+        req.body;
     try {
         const novo = await Cliente.create(
             { nome, email, senha, telefone, cpf, dataNascimento, endereco },
             { include: [Endereco] }
         );
-        
         res.status(201).json({ novo, message: "Cliente adicionado com sucesso" });
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Um erro aconteceu" });
     }
 });
 
-// GET
-router.get("/clientes", async (req, res) =>{
-    // SELECT * FROM clientes;
+// ROTA PARA LISTAR TODOS OS CLIENTES - GET
+router.get("/clientes", async (req, res) => {
     const listaClientes = await Cliente.findAll();
     res.json(listaClientes);
-})
+});
 
-// GET BY ID 
+// ROTA PARA LISTAR UM CLIENTE POR ID - GET
 router.get("/clientes/:id", async (req, res) => {
-    // SELECT * FROM clientes WHERE id = x
-
-    try{
+    try {
         const cliente = await Cliente.findOne({
-            where: {id: req.params.id},
-            include: [Endereco]
+            where: { id: req.params.id },
+            include: [Endereco],
         });
-    
-        if(cliente) {
+        if (cliente) {
             res.json(cliente);
-        }else{
+        } else {
             res.status(404).json({ message: "Usuário não encontrado" });
         }
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Um erro aconteceu" });
     }
 });
 
-//PUT -> Atualizar cliente;
-
+// ROTA PARA ATUALIZAR UM CLIENTE - PUT
 router.put("/clientes/:id", async (req, res) => {
-    const { nome, email, senha, telefone, cpf, dataNascimento, endereco } = req.body;
+    const { nome, email, senha, telefone, cpf, dataNascimento, endereco } =
+        req.body;
     const { id } = req.params;
-    try{
-        const atualizarCliente = await Cliente.findByPk( id );
-        if(atualizarCliente){
-            if(endereco){
-                await Endereco.update( endereco, {where: {clienteId: id}});
+    try {
+        const atualizarCliente = await Cliente.findByPk(id);
+        if (atualizarCliente) {
+            if (endereco) {
+                await Endereco.update(endereco, { where: { clienteId: id } });
             }
-            await atualizarCliente.update({ nome, email, senha, telefone, cpf, dataNascimento });
+            await atualizarCliente.update({
+                nome,
+                email,
+                senha,
+                telefone,
+                cpf,
+                dataNascimento,
+            });
             res.status(200).json({ message: "Cliente editado." });
         } else {
             res.status(404).json({ message: "Cliente não encontrado." });
         }
-    }catch (err) {
+    } catch (err) {
         console.error(err);
-    res.status(500).json({ message: "Um erro aconteceu." });
+        res.status(500).json({ message: "Um erro aconteceu." });
     }
 });
 
-
-
+// ROTA PARA DELETAR UM CLIENTE - DELETE
+router.delete("/clientes/:id", async (req, res) => {
+    const { id } = req.params;    
+    const cliente = await Cliente.findOne({ where: { id } });
+    try {
+        if (cliente) {
+            await cliente.destroy();
+            res.status(200).json({ message: "Cliente removido." });
+        } else {
+            res.status(404).json({ message: "Cliente não encontrado." });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+});
 
 
 module.exports = router;
