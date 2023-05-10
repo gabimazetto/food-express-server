1c7eb4ba297ea0b7e8b9ab3ef2f9bd1e2a7013b4
juliagascho <juliagascho@hotmail.com>
2023-05-10 09:46:11 -0300

    Rota para remover uma comida favorita finalizada - DELETE

--- routes/favoritos.js
+++ routes/favoritos.js
@@ -41,4 +41,21 @@ router.post("/favoritos/comidas", async (req, res) => {
     }
 });
 
+// ROTA PARA REMOVER UMA COMIDA FAVORITA - DELETE
+router.delete("/favoritos/comidas/:id", async (req, res) => {
+    const { id } = req.params;    
+    const favorito = await Favorito.findOne({ where: { id } });    
+    try {
+        if (favorito) {
+            await favorito.destroy();
+            res.status(200).json({ message: "Comida favorita removida." });
+        } else {
+            res.status(404).json({ message: "Comida favorita não encontrada." });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+});
+
 module.exports = router;
