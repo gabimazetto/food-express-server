49c30044eb35ba043692110abbae7fdd4388f697
juliagascho <juliagascho@hotmail.com>
2023-05-09 22:11:20 -0300

    Rota para remoção de avaliação

--- routes/avaliacaos.js
+++ routes/avaliacaos.js
@@ -38,5 +38,22 @@ router.get("/avaliacaos/:restauranteId", async (req, res) => {
     }
 });
 
+// ROTA PARA REMOVER UMA AVALIAÇÃO - DELETE
+router.delete("/avaliacaos/:id", async (req, res) => {
+    const { id } = req.params;    
+    const avaliacao = await Avaliacao.findOne({ where: { id } });
+    try {
+        if (avaliacao) {
+            await avaliacao.destroy();
+            res.status(200).json({ message: "Avaliação removida." });
+        } else {
+            res.status(404).json({ message: "Avaliação não encontrada." });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Um erro aconteceu." });
+    }
+});
+
 
 module.exports = router;
